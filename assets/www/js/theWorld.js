3aa95991ea258910195eaafd466dfa07f148e365
Ian Reah <ian.reah@gmail.com>
2013-07-25 21:49:38 +0100

    Added 3D model

--- assets/www/js/theWorld.js
+++ assets/www/js/theWorld.js
@@ -1,9 +1,13 @@
 var theWorld = {
     initialize: function() {
 		var tracker = new AR.Tracker("res/caffeine.wtc");
-		var overlay = new AR.Label("X", 1);
 		
-		new AR.Trackable2DObject(tracker, "target", {drawables: {cam: overlay}});
+		var model = new AR.Model("res/caffeine.wt3", {
+			scale: { x: 0.1, y: 0.1, z: 0.1 },
+			rotate: { tilt: -90.0, }
+		});
+		
+		new AR.Trackable2DObject(tracker, "target", {drawables: {cam: model}});
     }
 };
 
