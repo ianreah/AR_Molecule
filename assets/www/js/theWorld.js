086bb1e024f901a0c587a7b82c8ba4c61f9ef1f6
Ian Reah <ian.reah@nonlinear.com>
2013-07-26 12:19:42 +0100

    Added spinning animation

--- assets/www/js/theWorld.js
+++ assets/www/js/theWorld.js
@@ -1,9 +1,16 @@
 var theWorld = {
     initialize: function() {
-		var tracker = new AR.Tracker("res/caffeine.wtc");
-		
 		var model = new AR.Model("res/caffeine.wt3", {
 			scale: { x: 0.1, y: 0.1, z: 0.1 }
+		});
+		
+		var spinAnimation = new AR.PropertyAnimation(model, "rotate.roll",
+				0, 360, 10000, {type: AR.CONST.EASING_CURVE_TYPE.LINEAR});
+		
+		var tracker = new AR.Tracker("res/caffeine.wtc", {
+			onLoaded : function() {
+				spinAnimation.start(-1);
+			}
 		});
 		
 		new AR.Trackable2DObject(tracker, "target", {drawables: {cam: model}});
