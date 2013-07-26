ee6fa6d8d286ecc5ec4315f4de5b7336835b617f
Ian Reah <ian.reah@nonlinear.com>
2013-07-26 12:18:08 +0100

    Changed the model orientation so I don't have to rotate it in wikitude

--- assets/www/js/theWorld.js
+++ assets/www/js/theWorld.js
@@ -3,8 +3,7 @@ var theWorld = {
 		var tracker = new AR.Tracker("res/caffeine.wtc");
 		
 		var model = new AR.Model("res/caffeine.wt3", {
-			scale: { x: 0.1, y: 0.1, z: 0.1 },
-			rotate: { tilt: -90.0, }
+			scale: { x: 0.1, y: 0.1, z: 0.1 }
 		});
 		
 		new AR.Trackable2DObject(tracker, "target", {drawables: {cam: model}});
